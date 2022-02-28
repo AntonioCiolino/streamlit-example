9fd69a530c5d3efffcfaa8fed9c379de2f24a4f5
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 18:41:58 -0500

    test

---
+++ Writing.py
@@ -0,0 +1,45 @@
+import csv
+import random
+import streamlit as st
+import openai
+import Features
+import Tables
+
+class Writing:
+    def __init__(self):
+        openai.api_key=st.session_state.api_key
+
+
+    def completion(prompt, model, temp=0.73, top_p=1.0, tokens=500, freq_pen=1.73, pres_pen=0.43, stop=["END", "Scene:", "[Scene"]):
+        try:
+            # fine-tuned models requires model parameter, whereas other models require engine parameter
+            model_param = (
+                {"model": model}
+                if ":" in model
+                   and model.split(":")[1].startswith("ft")
+                else {"engine": model}
+            )
+
+            st.write("*** sent " + prompt)
+            response = openai.Completion.create(
+                prompt=prompt,
+                temperature=temp,
+                max_tokens=tokens,
+                top_p=top_p,
+                frequency_penalty=freq_pen,
+                presence_penalty=pres_pen,
+                stop=stop,
+                **model_param)
+            response = response['choices'][0]['text']
+            return response
+        except Exception as oops:
+            return st.write(str(oops))
+
+    def get_query(self, prompt):
+        try:
+            result = OAI.completion(prompt, model, temp=0.73, top_p=1.0, tokens=100, freq_pen=1.73, pres_pen=0.43, stop=["END", "Scene:", "[Scene"])
+            st.session_state.chapter += result
+            st.write(st.session_state.chapter)  # this is the text that is displayed on the page
+        except Exception as oops:
+            st.write('ERROR in get_query function:', oops)
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import openai
 
 import Features
 import Tables
+import Writing
 
 model = "curie:ft-vtcnp-2022-02-23-00-34-41"
 api_key = "enter your key here"
@@ -22,46 +23,11 @@ if 'api_key' not in st.session_state:
 if 'result' not in st.session_state:
     st.session_state.result = ""
 
-class OAI:
-    def completion(prompt, model, temp=0.73, top_p=1.0, tokens=500, freq_pen=1.73, pres_pen=0.43, stop=["END", "Scene:", "[Scene"]):
-        try:
-            # fine-tuned models requires model parameter, whereas other models require engine parameter
-            model_param = (
-                {"model": model}
-                if ":" in model
-                   and model.split(":")[1].startswith("ft")
-                else {"engine": model}
-            )
-
-            st.write("*** sent " + prompt)
-            response = openai.Completion.create(
-                prompt=prompt,
-                temperature=temp,
-                max_tokens=tokens,
-                top_p=top_p,
-                frequency_penalty=freq_pen,
-                presence_penalty=pres_pen,
-                stop=stop,
-                **model_param)
-            response = response['choices'][0]['text']
-            return response
-        except Exception as oops:
-            return st.write(str(oops))
-
-    def get_query(self, prompt):
-        try:
-            result = OAI.completion(prompt, model, temp=0.73, top_p=1.0, tokens=100, freq_pen=1.73, pres_pen=0.43, stop=["END", "Scene:", "[Scene"])
-            st.session_state.chapter += result
-            st.write(st.session_state.chapter)  # this is the text that is displayed on the page
-        except Exception as oops:
-            st.write('ERROR in get_query function:', oops)
-
-
 
 st.session_state.api_key = st.text_input('enter your api key here', st.session_state.api_key)
 prompt = st.text_input('Prompt to process', '')
 
-openAI = OAI()
+openAI = Writing()
 d = Tables.Tables()
 Features.Features()
 st.session_state.sel = st.sidebar.selectbox('Select a table', st.session_state.random_tables.keys())
@@ -73,6 +39,6 @@ st.session_state.chapter = st.text_area('edit this chapter', st.session_state.ch
 
 # call openAI
 if (st.session_state.api_key != "" and prompt != ""):
-    openai.api_key=st.session_state.api_key
+
     c_prompt = "create a " + st.session_state.feat + " from the following sentence.\n" + prompt + "\n---\n\n"
-    st.sidebar.button("execute query", on_click=openAI.get_query(c_prompt))
+    st.sidebar.button("execute query", on_click=writing.get_query(c_prompt))
