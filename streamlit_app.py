d79b5a264193a03270ea8f68887307d9488d2e01
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 12:17:52 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,11 @@ storydir = 'story'
 
 st.write(st.session_state.chapter)
 st.text_input(st.session_state.chapter)
+
+st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair, (...)
+     ''')
