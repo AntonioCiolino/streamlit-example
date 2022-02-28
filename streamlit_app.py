319ce1e7066192fd59124511b85d40cd20989079
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 10:49:27 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,8 @@ model = "curie:ft-vtcnp-2022-02-23-00-34-41"
 
 class Demo:
     random_tables = {}
-    selected_table = None
+
     def __init__(self, **kwargs):
-        self.random_tables = self.load_random_tables()
-
-
-    def load_random_tables(self):
         tables = {}
         with open('tables1e.csv') as data_file:
             data = csv.reader(data_file, delimiter='\t')
@@ -30,14 +26,13 @@ class Demo:
                     tables[current_table].append(row[1])
         return tables
 
-    def get_random_thing(self):
-        random_tables = self.load_random_tables()
-        st.write(self.selected_table)
+    def get_random_thing(self, selected_table):
+        st.write(selected_table)
         self.info = 'Getting random thing...'
 
-        st.write("Getting from " + self.selected_table)
+        st.write("Getting from " + selected_table)
         try:
-            result = random.choice(random_tables[self.selected_table])
+            result = random.choice(self.random_tables[selected_table])
             st.write(result)
         except Exception as oops:
             st.write('ERROR in get_random_thing function:', oops)
