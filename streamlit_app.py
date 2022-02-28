306adb50808e5f2832f3b051fa8842813915b064
AntonioCiolino <antonio.ciolino@gmail.com>
2022-02-27 19:01:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,12 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+import streamlit as st
+
+picture = st.camera_input("Take a picture")
+
+if picture:
+     st.image(picture)
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
