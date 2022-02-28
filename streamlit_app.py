ce243b47d3eef3d1548b259060fbacd9cb69906d
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 16:55:48 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,19 @@
 import streamlit as st
 import csv
 import random
+import openai
 
 model = "curie:ft-vtcnp-2022-02-23-00-34-41"
+api_key = "enter your key here"
+
 if 'random_tables' not in st.session_state:
     st.session_state.random_tables = {}
 if 'sel' not in st.session_state:
     st.session_state.sel = ""
 if 'chapter' not in st.session_state:
     st.session_state.chapter = "Example text goes here"
+if 'api_key' not in st.session_state:
+    st.session_state.api_key = ""
 
 class Tables:
     def __init__(self, **kwargs):
@@ -32,6 +37,31 @@ class Tables:
         except Exception as oops:
             st.write('ERROR in get_random_thing function:', oops)
 
+class OpenAI:
+    def completion(prompt, model, temp=0.73, top_p=1.0, tokens=500, freq_pen=1.73, pres_pen=0.43, stop=["END", "Scene:", "[Scene"]):
+        try:
+            # fine-tuned models requires model parameter, whereas other models require engine parameter
+            model_param = (
+                {"model": model}
+                if ":" in model
+                   and model.split(":")[1].startswith("ft")
+                else {"engine": model}
+            )
+
+            response = openai.Completion.create(
+                prompt=prompt,
+                temperature=temp,
+                max_tokens=tokens,
+                top_p=top_p,
+                frequency_penalty=freq_pen,
+                presence_penalty=pres_pen,
+                stop=stop,
+                **model_param)
+            response = response['choices'][0]['text']
+            return response
+        except Exception as oops:
+            return st.write(str(oops))
+
 
 d = Tables()
 d.load_tables()
@@ -41,4 +71,10 @@ storydir = 'story'
 
 st.write(st.session_state.chapter)
 st.session_state.chapter = st.text_input('edit this content', st.session_state.chapter)
+st.session_state.api_key = st.text_input('enter your api key here', st.session_state.api_key)
 
+# call openAI
+openai.api_key=st.session_state.api_key
+prompt = "create a " + "metaphor" + " from the following sentence.\n" + "i was very hungry" + "\n---\n\n"
+result = OpenAI.ompletion(prompt, "text-davinci-001").replace("\n", "").replace("---", "").replace("###", "")
+st.write (str(result))
