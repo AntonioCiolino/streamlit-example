ef86af42a4ceb137df523c636492f0e76e33fa40
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 08:56:41 -0500

    trying first push

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,46 +3,42 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import csv
+import random
+
+st.logging.basicConfig(filename='kivyUI.log', level=st.logging.INFO, filemode="w")
+st.logging.debug('Debugging started.')
+
+model = "curie:ft-vtcnp-2022-02-23-00-34-41"
+table_selected = ""
+
+def __init__(self, **kwargs):
+    random_tables = self.load_random_tables()
+
+    self.table_selected = st.selectbox('Select a table', random_tables.keys(), on_change=self.get_random_thing())
+    self.storydir = 'story'
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+ Find the table entries based not he keys that are in the select box
 """
+def load_random_tables(self):
+    tables = {}
+    with open('tables1e.csv') as data_file:
+        data = csv.reader(data_file, delimiter='\t')
+        for row in data:
+            if (row[0] == 'd100'):
+                current_table = row[1]
+                tables[current_table] = []
+            else:
+                tables[current_table].append(row[1])
+    return tables
 
-option = st.selectbox(
-     'How would you like to be contacted?',
-     ('Email', 'Home phone', 'Mobile phone'))
-
-st.write('You selected:', option)
-
-picture = st.camera_input("Take a picture")
-
-if picture:
-     st.image(picture)
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def get_random_thing(self, button):
+    self.info = 'Getting random thing...'
+    all_tables = self.load_random_tables()
+    try:
+        result = random.choice(all_tables[button.text])
+        st.write(result)
+    except Exception as oops:
+        print('ERROR in get_random_thing function:', oops)
+        st.logging.fatal('ERROR in get_random_thing function: %s', oops)
