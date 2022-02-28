9d5d9a6c1f5975ef8ee2ae1cd4b4038a055e1619
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 11:37:53 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ for i, v in enumerate(st.session_state.random_tables.keys()):
 for i, button in enumerate(buttons):
     if button:
         st.write(f"{i} button was clicked")
-        st.write(st.session_state.random_tables.keys().find(i.label))
+        st.write(st.session_state.random_tables.keys().values()[i])
 
 
 st.session_state.sel = st.sidebar.selectbox('Select a table', st.session_state.random_tables.keys(), on_change=d.get_random_thing, args=())
