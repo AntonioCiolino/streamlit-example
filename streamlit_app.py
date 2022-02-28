a60137cd72b0e6fc50a4b8a751ae8235cab1d79a
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 09:54:50 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
-import self
 import streamlit as st
 import csv
 import random
@@ -15,34 +14,38 @@ table_selected = ""
 
 st.write("started")
 
-random_tables = self.load_random_tables()
-st.write(random_tables)
+class Demo:
+    def __init__(self, **kwargs):
+        random_tables = self.load_random_tables()
+        st.write(random_tables)
 
-table_selected = st.selectbox('Select a table', random_tables.keys(), on_change=self.get_random_thing())
-storydir = 'story'
-st.write("init finished")
+        self.table_selected = st.selectbox('Select a table', random_tables.keys(), on_change=self.get_random_thing())
+        self.storydir = 'story'
+        st.write("init finished")
 
 
-def load_random_tables(self):
-    tables = {}
-    with open('tables1e.csv') as data_file:
-        data = csv.reader(data_file, delimiter='\t')
-        for row in data:
-            if (row[0] == 'd100'):
-                current_table = row[1]
-                tables[current_table] = []
-            else:
-                tables[current_table].append(row[1])
-    return tables
+    def load_random_tables(self):
+        tables = {}
+        with open('tables1e.csv') as data_file:
+            data = csv.reader(data_file, delimiter='\t')
+            for row in data:
+                if (row[0] == 'd100'):
+                    current_table = row[1]
+                    tables[current_table] = []
+                else:
+                    tables[current_table].append(row[1])
+        return tables
 
-def get_random_thing(self, button):
-    self.info = 'Getting random thing...'
-    all_tables = self.load_random_tables()
-    try:
-        result = random.choice(all_tables[button.text])
-        st.write(result)
-    except Exception as oops:
-        print('ERROR in get_random_thing function:', oops)
-        st.logging.fatal('ERROR in get_random_thing function: %s', oops)
+    def get_random_thing(self, button):
+        self.info = 'Getting random thing...'
+        all_tables = self.load_random_tables()
+        try:
+            result = random.choice(all_tables[button.text])
+            st.write(result)
+        except Exception as oops:
+            print('ERROR in get_random_thing function:', oops)
+            st.logging.fatal('ERROR in get_random_thing function: %s', oops)
+
+
 
 st.write("done")
