53bc3f06555576c0147d8b629563ff49f8bd6b3d
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 11:26:16 -0500

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ d.load_tables()
 buttons = []
 
 for i in enumerate(st.session_state.random_tables.keys()):
-    buttons.append(st.button(str(i)))
+    buttons.append(st.button(str(i.value)))
 
 for i, button in enumerate(buttons):
     if button:
