07acd5ea5cd59a040b2ca1431f5b642057313736
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 18:56:48 -0500

    test

--- Writing.py
+++ Writing.py
@@ -38,7 +38,7 @@ class Writing:
 
         try:
             result = self.completion(prompt, self.model)
-            st.session_state.chapter += result
+            st.session_state.chapter += str(result)
             st.write(st.session_state.chapter)  # this is the text that is displayed on the page
         except Exception as oops:
             st.write('ERROR in get_query function:', oops)
