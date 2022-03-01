9f35f25633eef516cfc63778b119a585ca710c69
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 19:24:16 -0500

    test

--- Writing.py
+++ Writing.py
@@ -10,6 +10,7 @@ class Writing:
         openai.api_key=st.session_state.api_key
 
     def write(self, prompt, model, temp=0.73, top_p=1.0, tokens=500, freq_pen=1.73, pres_pen=0.43, stop=["END", "Scene:", "[Scene"]):
+        st.write(prompt)
         try:
             # fine-tuned models requires model parameter, whereas other models require engine parameter
             model_param = (
@@ -36,7 +37,6 @@ class Writing:
     def get_query(self, prompt):
         try:
             result = self.write(prompt, self.model)
-            st.write("*** got back " + result)
             st.session_state.chapter += result
             # st.write(st.session_state.chapter)  # this is the text that is displayed on the page
         except Exception as oops:
