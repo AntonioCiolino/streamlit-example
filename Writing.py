22ee2f56fa312845db63c87d96ac0ceb78c76314
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 19:14:40 -0500

    test

--- Writing.py
+++ Writing.py
@@ -30,6 +30,7 @@ class Writing:
                 presence_penalty=pres_pen,
                 stop=stop,
                 **model_param)
+            st.write("*** got back " + response)
             response = response['choices'][0]['text']
             return response
         except Exception as oops:
