210596beb276563ac309ed12c1294cde399a4572
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-01 16:53:06 -0500

    selection and prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,6 @@ else:
     #completions vs. tuning.
     if (st.sidebar.button('Run tuned content', help="Calls OpenAI for fine tuned content.")):
         st.session_state.chapter += Writing.Writing().completeModel(st.session_state.chapter, model)
-        chapter = st.session_state.chapter
         chapter = st_quill(st.session_state.chapter)
     elif (st.sidebar.button('Run generic content', help="Calls OpenAI for classic DaVinci content.")):
         st.session_state.chapter += Writing.Writing().completeDavinci(st.session_state.chapter)
@@ -79,6 +78,7 @@ else:
         if (chapter != st.session_state.chapter):
             st.success("Updated Content")
             st.session_state.chapter = chapter
+            chapter = st_quill(st.session_state.chapter )
 
     st.write(chapter)
     st.write(st.session_state.chapter)
