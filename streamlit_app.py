c670df271f3a3bc159d38b7926844a2b56465e03
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-01 16:18:03 -0500

    selection and prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,6 @@ else:
     if (st.sidebar.button('Run generic content', help="Calls OpenAI for classic DaVinci content.")):
         st.session_state.chapter += Writing.Writing().completeDavinci(st.session_state.chapter)
 
-
     if (st_quill):
         chapter = st_quill()
         if (chapter != st.session_state.chapter):
