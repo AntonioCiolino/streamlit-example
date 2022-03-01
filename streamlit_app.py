9cbe4cdc62a680a8f5e478cc91b0909d42e01cb9
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-01 16:27:46 -0500

    selection and prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,19 +63,18 @@ else:
         st.session_state.chapter += Writing.Writing().get_generic_content(prompt)
 
     st.sidebar.info("Use the content box to enhance chapter content. Note that this takes the whole chapter; we do not handle highlighting and custom selection yet.")
+    chapter = st_quill()
+    if (chapter != st.session_state.chapter):
+        st.success("Updated Content")
+        st.session_state.chapter = chapter
+
     #completions vs. tuning.
     if (st.sidebar.button('Run tuned content', help="Calls OpenAI for fine tuned content.")):
-        st.write("Starting with content: " + st.session_state.chapter)
         st.session_state.chapter += Writing.Writing().completeModel(st.session_state.chapter, model)
-        st.write("ended with content: " + st.session_state.chapter)
-        chapter = st_quill(value = st.session_state.chapter)
     elif (st.sidebar.button('Run generic content', help="Calls OpenAI for classic DaVinci content.")):
         st.session_state.chapter += Writing.Writing().completeDavinci(st.session_state.chapter)
-        chapter = st_quill(value = st.session_state.chapter)
-    else:
-        chapter = st_quill()
-        if (chapter != st.session_state.chapter):
-            st.success("Updated Content")
-            st.session_state.chapter = chapter
+
+
+
 
 
