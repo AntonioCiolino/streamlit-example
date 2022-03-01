e5687e5a492f981b2779e17b892d771a62e3e9d2
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-01 17:04:32 -0500

    selection and prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ else:
             st.session_state.chapter += Writing.Writing().completeDavinci(st.session_state.chapter)
 
         st.write("checking quill content")
-        chapter = st_quill()
+        chapter = st_quill(value = st.session_state.chapter )
         if (chapter != st.session_state.chapter):
             st.success("Updated Content")
             st.session_state.chapter = chapter
