d027287412ce9e622e4daec6f3a4f8c792505951
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-01 09:01:22 -0500

    selection and prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ thing = st.sidebar.button('Get random thing', on_click=Tables.Tables().get_rando
 st.session_state.feat = st.sidebar.selectbox('Select a feature', st.session_state.features)
 
 chapter = st.text_area('edit this chapter', st.session_state.chapter)
-
+st.write(chapter)
 
 # call openAI
 if (st.session_state.api_key != "" and prompt != ""):
