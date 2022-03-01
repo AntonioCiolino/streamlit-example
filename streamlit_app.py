b2a81de40ca053d3ec84663d711b876edbd69486
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-01 11:20:40 -0500

    selection and prompts

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ prompt = st.text_input('Prompt to process', '')
 st.session_state.features = Features.Features.features
 st.session_state.random_tables = Tables.Tables().random_tables
 
-st.session_state.sel = st.sidebar.selectbox('Select a table', st.session_state.random_tables.keys())
+st.sidebar.info("Get a random string from the tables.")
+st.session_state.sel = st.sidebar.selectbox('Create random table content', st.session_state.random_tables.keys())
 
 # detemine button stuff before displaying or loading text boxes
 if st.sidebar.button('Get random thing'):
