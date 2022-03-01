36fc4b5bdc69f12bac4b410f86ec634282c75247
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-02-28 19:06:15 -0500

    test

--- Writing.py
+++ Writing.py
@@ -10,8 +10,8 @@ class Writing:
         openai.api_key=st.session_state.api_key
 
     def write(prompt, model, temp=0.73, top_p=1.0, tokens=500, freq_pen=1.73, pres_pen=0.43, stop=["END", "Scene:", "[Scene"]):
-        st.write(model)
         try:
+            st.write(model)
             # fine-tuned models requires model parameter, whereas other models require engine parameter
             model_param = (
                 {"model": model}
