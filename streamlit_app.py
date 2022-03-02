5a3c0c11b986950e789a59fc69b9641137ea4823
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-02 11:47:05 -0500

    format changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,9 @@ if 'models' not in st.session_state:
 if 'chapter' not in st.session_state:
     st.session_state.chapter = ""
 
-with st.expander("Enter your API Key"):
-    st.session_state.api_key = st.text_input('API Key', st.session_state.api_key)
+if (st.session_state.api_key == ""):
+    with st.expander("Enter your API Key"):
+        st.session_state.api_key = st.text_input('API Key', st.session_state.api_key)
 
 if (st.session_state.api_key == ""):
     st.write("You need to enter your API Key to use this app.")
@@ -54,7 +55,7 @@ else:
         st.caption("choose which model that OpenAI will use to generate your content. Choose DaVinci, Curie, or your own fine tuned models.")
         model = st.selectbox("Select a model", st.session_state.models)
 
-    with st.expander("Tool: Inject random data"):
+    with st.expander("Optional Tool: Inject random data"):
         st.caption("Appends a random thing from the collection of options into the story area. This can be used to spark ideas for yourself or the generator.")
         st.session_state.sel = st.selectbox('Select grouping of content', st.session_state.random_tables.keys(),
                                             help="Select a random table to generate content from.")
@@ -63,7 +64,7 @@ else:
         if st.button('Inject a thing', help="Add a random thing to the content from a list of items."):
             st.session_state.chapter += "\n" + Tables.Tables().get_random_thing()
 
-    with st.expander("Optional prompt: generate a style based on a specific sentence, phrase or idea."):
+    with st.expander("Optional tool: generate a style based on a specific sentence, phrase or idea."):
         prompt = st.text_input('Prompt to process', '', help="If you have a specific short prompt, place it here to process. It will append the results to the story.")
 
         # for the prompt, if the prompt is blank, disable the controls, but still render.
